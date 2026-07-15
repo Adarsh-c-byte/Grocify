601b8b3833073c1216caadff451719efd6692602
Adarsh-c-byte <vsgs55409@gmail.com>
2026-07-15 23:41:49 +0530

    add banner to Fruit commonet

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import Home from './components/Home/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Fruits from './components/Fruits/Fruits'
-import Dairy from './components/Dairy/Dairy'
-import SeaFood from './components/SeaFood/SeaFood'
-import AllProducts from './components/AllProducts/AllProducts'
+import React from "react";
+import Home from "./components/Home/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Fruits from "./components/Fruits/Fruits";
+import Dairy from "./components/Dairy/Dairy";
+import SeaFood from "./components/SeaFood/SeaFood";
+import AllProducts from "./components/AllProducts/AllProducts";
+import Layot from "./components/Layot/Layot";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layot />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/fruits",
+          element: <Fruits />,
+        },
+        {
+          path: "/dairy",
+          element: <Dairy />,
+        },
+        {
+          path: "/seafood",
+          element: <SeaFood />,
+        },
+        {
+          path: "/allproducts",
+          element: <AllProducts />,
+        }
+      ],
+    },
+  ]);
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/fruits",
-    element: <Fruits />
-  },
-  {
-    path: "/dairy",
-    element : <Dairy />
-  },
-  {
-    path: "/seafood",
-    element: <SeaFood />
-  },
-  {
-    path : "/allproducts",
-    element : <AllProducts />
-  }
-])
+  return <RouterProvider router={router} />;
+};
 
-return <RouterProvider router={router}/>
-
-}
-
-export default App
+export default App;
--- src/components/Fruits/Fruits.jsx
+++ src/components/Fruits/Fruits.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from '../Banner/Banner'
 
 const Fruits = () => {
   return (
     <div>
-      <h1>i am fruits</h1>
+      <Banner />
     </div>
   )
 }
